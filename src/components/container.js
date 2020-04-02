796c56729c5c34164b06785d568dc80032e2e920
王漫 <wangman@wangmandeMac-mini.local>
2020-04-02 03:41:08 -0700

    1232

--- src/components/container.js
+++ src/components/container.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
-import { Button, Table, MessageBox, Message } from "element-react";
-import EditDialog from "./editDialog";
+import { MessageBox, Message } from "element-react";
+import TableCom from "./tableCom";
 import "element-theme-default";
 // 主容器
 class Container extends React.Component {
@@ -10,12 +10,19 @@ class Container extends React.Component {
     this.state = {
       data: [],
       editDialogVisible: false,
-      id: ""
+      form: {
+        id: "",
+        name: "",
+        author: "",
+        pubDate: ""
+      },
+      index: -1
     };
     this.showEditDialog = this.showEditDialog.bind(this);
     this.showDelDialog = this.showDelDialog.bind(this);
     this.handleEditCancel = this.handleEditCancel.bind(this);
     this.handleEditConfirm = this.handleEditConfirm.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   }
   componentDidMount() {
     // 从books.json文件中获取书籍的数据
@@ -30,35 +37,46 @@ class Container extends React.Component {
         console.log(err);
       });
   }
+  componentWillReceiveProps() {
+    console.log(this.state.form);
+    console.log(this.state.index);
+
+    if (this.state.index !== -1) {
+      const obj = this.state.data[this.state.index];
+      this.setState({
+        form: obj
+      });
+    }
+    // return true;
+  }
   // 显示编辑对话框
   showEditDialog(id) {
+    const index = this.state.data.findIndex(v => {
+      return v.id === id;
+    });
     this.setState({
       editDialogVisible: true,
-      id: id
+      index: index,
+      form: this.state.data[index]
     });
   }
   // 显示删除消息弹框
   showDelDialog(id) {
     //
-    const delId = this.state.data.findIndex(v => {
-      return v.id === id;
-    });
-    let arr = this.state.data;
-    arr.splice(delId, 1);
-
     MessageBox.confirm("此操作将永久删除该文件, 是否继续?", "提示", {
       type: "warning"
     })
       .then(() => {
-        this.setState(prevState => {
-          delete prevState.data;
-          return prevState;
+        // 点击确认删除后，查找编号所对应的索引
+        const delId = this.state.data.findIndex(v => {
+          return v.id === id;
         });
+        let arr = this.state.data;
+        // 删除后重新赋值
+        arr.splice(delId, 1);
         this.setState({
-          data: arr
+          data: [...arr]
         });
-        console.log(this.state.data);
-
         Message({
           type: "success",
           message: "删除成功!"
@@ -74,108 +92,38 @@ class Container extends React.Component {
   // 取消编辑操作
   handleEditCancel() {
     this.setState({
-      editDialogVisible: false,
-      id: ""
+      editDialogVisible: false
+    });
+  }
+  handleFormChange(key, value) {
+    console.log(key, value);
+
+    const formObj = this.state.form;
+    formObj[key] = value;
+    this.setState({
+      form: formObj
     });
   }
   // 修改表单确认操作
-  handleEditConfirm(form) {
-    const arr = this.state.data;
-    const index = this.state.data.findIndex(v => {
-      return v.id === form.id;
-    });
-    arr[index] = form;
-    // 通过将组件数据清空,然后再赋值的操作实现页面重新渲染
-    this.setState(
-      {
-        data: []
-      },
-      () => {
-        this.setState({
-          data: arr,
-          editDialogVisible: false,
-          id: ""
-        });
-      }
-    );
+  handleEditConfirm(value, flag) {
+    // console.log(value);
+    // console.log(flag);
   }
+
   render() {
     return (
       <TableCom
         data={this.state.data}
-        editId={this.state.id}
         editDialogVisible={this.state.editDialogVisible}
         showEditDialog={this.showEditDialog}
         showDelDialog={this.showDelDialog}
         handleEditCancel={this.handleEditCancel}
         handleEditConfirm={this.handleEditConfirm}
+        handleFormChange={this.handleEditConfirm}
+        form={this.state.form}
       />
     );
   }
-}
-function TableCom(props) {
-  let columns = [
-    {
-      label: "编号",
-      prop: "id",
-      width: 180
-    },
-    {
-      label: "书名",
-      prop: "name",
-      width: 180
-    },
-    {
-      label: "作者",
-      prop: "author"
-    },
-    {
-      label: "出版日期",
-      prop: "pubDate"
-    },
-    {
-      label: "操作",
-      prop: "id",
-      render: function(data) {
-        return (
-          <span>
-            <Button
-              plain={true}
-              type="info"
-              size="small"
-              onClick={props.showEditDialog.bind(this, data["id"])}
-            >
-              编辑
-            </Button>
-            <Button
-              type="danger"
-              size="small"
-              onClick={props.showDelDialog.bind(this, data["id"])}
-            >
-              删除
-            </Button>
-          </span>
-        );
-      }
-    }
-  ];
-  return (
-    <div>
-      <Table
-        style={{ width: "100%" }}
-        columns={columns}
-        data={props.data}
-        border={true}
-      />
-      <EditDialog
-        dialogVisible={props.editDialogVisible}
-        handleCancel={props.handleEditCancel}
-        handleEditConfirm={props.handleEditConfirm}
-        data={props.data}
-        editId={props.editId}
-      />
-    </div>
-  );
 }
 
 export default Container;
--- src/components/editDialog.js
+++ src/components/editDialog.js
@@ -1,105 +1,99 @@
 import React from "react";
 import { Button, Dialog, Form, Input } from "element-react";
 class EditDialog extends React.Component {
+  // let formValue = {
+  //   id: props.form.id,
+  //   name: props.form.name,
+  //   author: props.form.author,
+  //   pubDate: props.form.pubDate
+  // };
   constructor(props) {
     super(props);
+    console.log(props.form);
+
     this.state = {
-      form: {
-        id: "",
-        name: "",
-        author: "",
-        pubDate: ""
-      },
-      // 验证规则
-      rules: {
-        name: [{ required: true, message: "请输入书籍名称", trigger: "blur" }],
-        author: [{ required: true, message: "请输入作者", trigger: "blur" }],
-        pubDate: [
-          { required: true, message: "请输入出版时间", trigger: "blur" }
-        ]
-      }
+      formValue: {}
     };
-    this.formRef = React.createRef();
   }
-  // 处理确定
-  handleSubmit = e => {
-    e.preventDefault();
-    // 验证表单，只有当表单验证规则通过后才能进行确认操作
-    this.formRef.current.validate(valid => {
-      if (valid) {
-        let form = this.state.form;
-        form.id = this.props.editId;
-        this.setState({
-          form: form
-        });
-        this.props.handleEditConfirm(this.state.form);
-      } else {
-        console.log("error submit!!");
-        return false;
-      }
-    });
+  // 当表单改变时修改state中的表单数据
+  onChange = (key, value) => {
+    console.log(key, value);
+
+    this.props.onFormChange(key, value);
   };
-  // 处理取消
+  //当表单重置
+  handleReset = e => {
+    e.preventDefault();
+    // formValue = {
+    //   id: this.state.form.id,
+    //   author: "",
+    //   name: "",
+    //   pubDate: ""
+    // };
+  };
+  validateForm = e => {
+    // 验证表单，只有当表单都填才能确认
+    if (
+      this.state.formValue.name.length !== 0 &&
+      this.state.formValue.author.length !== 0 &&
+      this.state.formValue.pubDate.length !== 0
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  handleConfirm = e => {
+    e.preventDefault();
+    let flag = this.validateForm();
+    this.props.handleEditConfirm(this.state.formValue, flag);
+  };
   handleCancel = e => {
     e.preventDefault();
-
     this.props.handleCancel();
-    //   重置表单
-    this.formRef.current.resetFields();
   };
-
-  onChange(key, value) {
-    this.setState({
-      form: Object.assign({}, this.state.form, { [key]: value })
-    });
-  }
   render() {
     return (
-      <div>
-        <Dialog
-          title="编辑书籍"
-          size="tiny"
-          visible={this.props.dialogVisible}
-          onCancel={this.props.handleCancel}
-          lockScroll={false}
-        >
-          <Dialog.Body>
-            <Form
-              ref={this.formRef}
-              model={this.state.form}
-              rules={this.state.rules}
-            >
-              <Form.Item label="编号">
-                <Input disabled value={this.props.editId}></Input>
-              </Form.Item>
-              <Form.Item label="书名" prop="name">
-                <Input
-                  value={this.state.form.name}
-                  onChange={this.onChange.bind(this, "name")}
-                ></Input>
-              </Form.Item>
-              <Form.Item label="作者" prop="author">
-                <Input
-                  value={this.state.form.author}
-                  onChange={this.onChange.bind(this, "author")}
-                ></Input>
-              </Form.Item>
-              <Form.Item label="出版日期" prop="pubDate">
-                <Input
-                  value={this.state.form.pubDate}
-                  onChange={this.onChange.bind(this, "pubDate")}
-                ></Input>
-              </Form.Item>
-            </Form>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Button onClick={this.handleCancel}>取消</Button>
-            <Button type="primary" onClick={this.handleSubmit}>
-              确定
-            </Button>
-          </Dialog.Footer>
-        </Dialog>
-      </div>
+      <Dialog
+        title="编辑书籍"
+        size="tiny"
+        visible={this.props.dialogVisible}
+        onCancel={this.props.handleCancel}
+        lockScroll={false}
+      >
+        <Dialog.Body>
+          <Form>
+            <Form.Item label="编号">
+              <Input disabled value={this.state.formValue.id}></Input>
+            </Form.Item>
+            <Form.Item label="书名">
+              <Input
+                onChange={this.onChange.bind(this, "name")}
+                value={this.state.formValue.name}
+              ></Input>
+            </Form.Item>
+            <Form.Item label="作者">
+              <Input
+                onChange={this.onChange.bind(this, "author")}
+                value={this.state.formValue.author}
+              ></Input>
+            </Form.Item>
+            <Form.Item label="出版日期">
+              <Input
+                onChange={this.onChange.bind(this, "pubDate")}
+                value={this.state.formValue.pubDate}
+              ></Input>
+            </Form.Item>
+          </Form>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Button onClick={this.handleReset}>重置</Button>
+          <Button onClick={this.handleCancel}>取消</Button>
+          <Button type="primary" onClick={this.handleConfirm}>
+            确定
+          </Button>
+        </Dialog.Footer>
+      </Dialog>
     );
   }
 }
